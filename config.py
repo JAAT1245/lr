cb0cb8d505ca8c338652311b9d4ed3d4617836ff
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-12-26 05:47:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "22354271"))
+API_HASH = getenv("API_HASH", "957a4eeb4d05144bd6c1838a44d7f1d6")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "902551614").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://vpsrestrictedsaver:AWSrestrictedsaver@cluster0.a3ejs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002374822952")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002182291545"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
